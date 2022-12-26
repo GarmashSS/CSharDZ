91fcc85faca1974865d1b907fceb067f1a659dc0
GarmashSS <120036470+GarmashSS@users.noreply.github.com>
2022-12-27 01:28:57 +0300

    Create Program.cs

--- DZ2_1/Program.cs
+++ DZ2_1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+//918 -> 1
+
+
+Console.WriteLine ("Введите трехзначное число");
+ int num = Convert.ToInt32(Console.ReadLine());
+ int res = (num /10 ) % 10;
+ 
+ if (num < 0)
+ {
+    num = -num;
+ }
+if (num > 100 && num < 1000 )
+{
+ Console.WriteLine($"Вторая цифра числа : {res}");
+} else {
+    Console.WriteLine($"Число не соотвествует заданному требованию");
+}
+
+
